3466efcceb984366840e28841b1614b1d1794e70
Bladi452 <cleto662@gmail.com>
2021-05-04 13:07:58 -0400

    Archivos de sistema de navegacion terminado (4 May)

--- app/navegacion/Navegacion.js
+++ app/navegacion/Navegacion.js
@@ -1,10 +1,2 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
-export default function Navegacion() {
-    return(
-    <View>
-    
-    </View>
-    )
-}
+import {} from ''
