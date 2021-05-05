1ed72c42af312488c31bc33976c44d374fa80e73
daniperezdpj <daniperez.dpj@gmail.com>
2021-05-05 16:33:09 -0400

    Intento de login

--- app/screens/Cuenta/Login.js
+++ app/screens/Cuenta/Login.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text, TextInput, StyleSheet,Dimensions} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
 import {Button} from 'react-native-elements'
 
-
+const {width: WIDTH} = Dimensions.get('window')
 export default function Login (){
     return (
         <View>
-            <Text>Login</Text>
+         <TextInput  style={styles.input}
+          placeholder= "Username"
+          placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+          underlineColorAndroid='transparent'/>
+            
+            
+         <TextInput  style={styles.input} 
+          placeholder= "Password" />
+        
             <BotonForm/>
+            
         </View>
     )
 }
@@ -16,7 +25,7 @@ export default function Login (){
 function BotonForm(){
 const navigate = useNavigation();
     return(
-        
+      
         <Button
         title="Login"
         onPress={()=>navigate.navigate("Seleccion")}
@@ -24,3 +33,16 @@ const navigate = useNavigation();
 
     )
 }
+const styles = StyleSheet.create({
+  input: {
+    width: WIDTH -55,
+    height: 40,
+    margin: 12,
+   borderRadius: 45,
+fontSize: 16,
+paddingLeft: 45,
+backgroundColor: 'rgba(0, 0, 0, 0.35)',
+color: 'rgba(255, 255, 255, 0.7)',
+marginHorizontal: 25
+  },
+});
