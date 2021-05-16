0d87f8627a59246a7ed316da77c8940f6f9c4021
bladi452 <cleto662@gmail.com>
2021-05-15 21:28:48 -0400

    Registro en proceso

--- app/screens/Cuenta/Registro.js
+++ app/screens/Cuenta/Registro.js
@@ -1,9 +1,113 @@
-import React from 'react'
-import {View,Text, TextInput, StyleSheet,Dimensions,KeyboardAvoidingView,ScrollView} from 'react-native'
+import React, {useState} from 'react';
+import {View,Text, TextInput, StyleSheet, TouchableOpacity,KeyboardAvoidingView,ScrollView,Platform} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
 import {Button} from 'react-native-elements'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Input } from 'react-native-elements/dist/input/Input';
+
+export default function Registro (){
+
+  return(
+<ScrollView>    
+    <View style={styles.vista}>
+      <TextInput style={styles.Input}
+      placeholder = "Nombre"
+    
+    />
+
+      <TextInput style={styles.Input}
+      placeholder = "Apellidos"
+    
+    />
+
+      <TextInput style={styles.Input}
+      placeholder = "Email"
+    
+    />
+  <Calendario/>
+    </View>
+    </ScrollView>
+  )
+
+}
 
 
+function Calendario () {
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+  
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+  
+  const showDatepicker = () => {
+    showMode('date');
+  };
+    
+  return(
+<View>
+      <View>
+      < TouchableOpacity
+          onPress={showDatepicker} 
+          style={[styles.sesion, {
+            backgroundColor:"#EFAF4F"
+          }]}
+          >
+  
+  <Text style={{ marginTop:11,fontSize: 15, color: '#fff'}}>
+    Fecha de Nacimiento
+    </Text>
+          </TouchableOpacity>
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+ Input:{
+   backgroundColor:"#ffffff",
+   borderColor:"#000000",
+   borderWidth:1,
+   marginTop:40,
+   alignSelf:"center",
+   width:300,
+   height:30,
+    borderRadius:10
+  },
+
+  sesion:{
+    alignItems:"center",
+    height:45,
+    width:300,
+    borderRadius:8,
+    alignSelf:"center",
+    marginTop:15
+  },
+  vista:{
+    backgroundColor:"#08D5B9"
+  }
+  
+})
+
+/*
 const {width: WIDTH} = Dimensions.get('window')
 export default function Login (){
   
@@ -11,7 +115,7 @@ export default function Login (){
       <KeyboardAvoidingView behavior= "padding">
         <ScrollView style={styles.container}>
       <View>
-         <TextInput  style={styles.input}
+             <TextInput  style={styles.input}
           placeholder= "Nombres"
           placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
           underlineColorAndroid='transparent'/>
@@ -20,7 +124,8 @@ export default function Login (){
           placeholder= "Apellidos"
           placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
           underlineColorAndroid='transparent'/>
-            <TextInput  style={styles.input}
+            
+              <TextInput  style={styles.input}
           placeholder= "Email"
           placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
           underlineColorAndroid='transparent'/>
@@ -97,3 +202,5 @@ marginHorizontal: 25
      marginTop:20
   }
 });
+
+*/
