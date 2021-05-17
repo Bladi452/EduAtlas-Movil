a70f36f72dd383777c0561a46f8b80ebf44c24cd
bladi452 <cleto662@gmail.com>
2021-05-17 17:15:57 -0400

    Max Henriquez funcional informaciones

--- app/screens/Inscripcion/InfoSelect/MaxHenriquez.js
+++ app/screens/Inscripcion/InfoSelect/MaxHenriquez.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
-import {View, Text, Image,StyleSheet} from 'react-native'
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
+import MapView,{Marker} from 'react-native-maps'
 
 export default function MaxHenriquez (){
-   const navigate = useNavigation()
-    
+   const navigation = useNavigation()
 
  return(
     <ScrollView style={styles.container}>
@@ -38,27 +38,67 @@ export default function MaxHenriquez (){
             <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Electronica.</Text>
             
         <Text style={[styles.textsec,{
-      backgroundColor:"#ffffff",
-      fontSize:16,
+      color:"#000000",
+      fontSize:18,
       marginTop:11,
-      marginBottom:22,
+      marginBottom:13,
     }]}>Ubicacion</Text>
+
+    <MapView style={styles.map} initialRegion={{latitude:18.526341919411482, longitude:-70.05271297194588, latitudeDelta:0.09, longitudeDelta:0.0921 }}>
+<Marker
+  coordinate={{ latitude : 18.525286036836068 , longitude : -70.05256742347363 }} title="Max Henriquez"
+/>
+    </MapView>
+  
+    <TouchableOpacity
+        onPress={()=>navigation.navigate("Espera")}
+        style={[styles.sesion, {
+          backgroundColor:"#EFAF4F"
+        }]}>
+        <Text style={{ marginTop:11,fontSize: 15, color: '#fff'}}>Enviar Solicitud</Text>
+      </TouchableOpacity>
+  
         </View>
     </ScrollView>
-)
+  )
 }
+
+
 const styles = StyleSheet.create({
+con:{
+  position:'absolute',
+  top:0,
+  left:0,
+  right:0,
+  bottom:0
+},
+
+map:{
+  width: 312.5,
+  height: 103.3,
+  alignSelf:"center"
+},
+
+sesion:{
+  alignItems:"center",
+  height:45,
+  width:300,
+  borderRadius:8,
+  alignSelf:"center",
+  marginTop: 20
+},
 
 textsec:{
     textAlign:"center",
     fontWeight:"bold",
   },
-  container:{
+
+container:{
     backgroundColor:"#08D5B9",
     height:"100%"
   },
 
-  logo: {
+logo: {
     width:122.1,
     height: 125,
     alignSelf :"center",
@@ -69,6 +109,6 @@ textsec:{
     color:"#ffffff",
     alignSelf:"center",
     fontWeight:"bold",
-    marginBottom:4
+    marginBottom:2
   }
 })
