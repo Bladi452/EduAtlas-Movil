1cd964c9b391f2b02d1e3e892867700d7d61c677
bladi452 <cleto662@gmail.com>
2021-05-17 15:23:30 -0400

    Max Henriquez casi terminado

--- app/screens/Inscripcion/InfoSelect/MaxHenriquez.js
+++ app/screens/Inscripcion/InfoSelect/MaxHenriquez.js
@@ -1,41 +1,74 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
 import {View, Text, Image,StyleSheet} from 'react-native'
-import {Button} from 'react-native-elements'
 import {useNavigation} from '@react-navigation/native'
+
 export default function MaxHenriquez (){
    const navigate = useNavigation()
     
 
  return(
-    <ScrollView>
+    <ScrollView style={styles.container}>
         <View>
-            
+
+<Text style={styles.titulo}>EduAtlas</Text>
+
 <Image source={require("../../../../assets/Imagen.png")}
     style={styles.logo}
 />
             
-        <Text style={styles.textsec}>Politécnico Max Henriquez Ureña</Text>
+        <Text 
+        style={[styles.textsec,{
+    fontSize:18,
+    color:"#ffffff",
+    marginTop:11,
+    marginBottom:20,
+}]}>Politécnico Max Henriquez Ureña</Text>
     
+    <Text style={[styles.textsec,{
+      fontSize:14,
+      marginTop:11,
+      marginBottom:20,
+    }]}>El centro se encuentra operando desde el año 2000, nos enfocamos en dar excelencia en nuestros estudiantes capacitandolos en las siguientes areas Tecnicas:</Text>
+            <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Electricidad.</Text>
+            <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Refrigeracion.</Text>
+            <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Informatica.</Text>
+            <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Logistica.</Text>
+            <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Contabilidad.</Text>
+            <Text style={[styles.textsec,{fontSize:15, color:"#ffffff"}]}>Electronica.</Text>
             
+        <Text style={[styles.textsec,{
+      backgroundColor:"#ffffff",
+      fontSize:16,
+      marginTop:11,
+      marginBottom:22,
+    }]}>Ubicacion</Text>
         </View>
     </ScrollView>
 )
 }
-
 const styles = StyleSheet.create({
+
 textsec:{
-    alignSelf:"center",
-    marginTop:11,
-    marginBottom:20,
+    textAlign:"center",
     fontWeight:"bold",
-    fontSize:25,
+  },
+  container:{
+    backgroundColor:"#08D5B9",
+    height:"100%"
   },
 
   logo: {
-    marginTop:0, 
-    width:50 ,
-    height: 50,
+    width:122.1,
+    height: 125,
     alignSelf :"center",
   },
+
+  titulo:{
+    fontSize:62,
+    color:"#ffffff",
+    alignSelf:"center",
+    fontWeight:"bold",
+    marginBottom:4
+  }
 })
